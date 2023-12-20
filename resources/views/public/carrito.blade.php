765fe72ba46cc6ff97af132559b262c8cfe433bf
wirovi07 <wirovi07>
2023-12-20 11:39:28 -0500

    Incremento - Decremento - EliminarProductoCarrito

--- resources/views/public/carrito.blade.php
+++ resources/views/public/carrito.blade.php
@@ -3,14 +3,14 @@
 @section('contenido')
     <div class="row ">
 
-        <div id="productocarrito" class="col-md-8">
+        <div id="productocarrito" class="col-md-9">
             <div class="card">
                 <div class="card-header">
                     Productos
                 </div>
                 <div class="card-body p-5 row">
                     <div class="row">
-                        <div class="flex-item mb-4 me-3 d-flex" v-for="producto in cart">
+                        <div class="flex-item justify-content-between mb-4 me-3 d-flex" v-for="producto in cart">
                             <div class="col-md-6 d-flex ">
                                 <a :href="'/detalle/' + producto.id">
                                     <img style="width: 64px; height: 64px;" :src="producto.imagen" class="card-img-top"
@@ -23,21 +23,19 @@
                                         @{{ truncateText(producto.nombre, 50) }}
                                     </span>
 
-                                    <div class="style-principla-words">
-                                        <span class="style-words">Eliminar</span>
+                                    <div class="style-principla-words cursor-pointer">
+                                        <span class="style-words" @click="removeFromCart(producto)">Eliminar</span>
                                         <span class="style-words">Guardar</span>
                                         <span class="style-words">comprar ahora</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-1 botton-increm mb-xxl-5">
-                                <div class="text-center">
-                                    <span onclick="decrementarCantidad()">-</span>
-                                    <input class="botton-increm-input" type="text" id="cantidad" value="1"
-                                        style="width: 30px; text-align: center; border: none" readonly>
-                                    <span onclick="incrementarCantidad()">+</span>
-                                </div>
-                            </div>
+                            <div class="col-md-4 botton-increm text-center" style="width: 100px; height: 32px; display: flex; align-items: center; justify-content: center; cursor: pointer"> 
+                                <span style="font-size: 26px; font-weight: 400; color: rgba(0, 0, 0, .55);" @click="decrementarCantidad(producto)">-</span>
+                                <input class="botton-increm-input" type="text" v-model="producto.cantidad"
+                                    style="width: 30px; text-align: center; border: none; font-size: 18px" readonly>
+                                <span style="font-size: 22px; color: #3483fa; font-weight: 500" @click="incrementarCantidad(producto)">+</span>
+                            </div>                                                    
                         </div>
                     </div>
                 </div>
@@ -46,7 +44,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card">
                 <div class="">
                     hola
@@ -60,28 +58,23 @@
     <script>
         var vue_app = new Vue({
             el: '#productocarrito',
-            created() {
-                this.loadCart()
+            mounted() {
+                this.loadCart();
             },
             data: {
                 cart: []
             },
             methods: {
                 formatPrecio: function(precio) {
-                    // Asegurar que el precio sea un número
                     const precioNumerico = parseFloat(precio);
 
-                    // Verificar si es un número válido
                     if (!isNaN(precioNumerico)) {
-                        // Formatear como moneda colombiana
                         return Math.round(precioNumerico).toLocaleString('es-CO', {
                             style: 'currency',
                             currency: 'COP',
                             minimumFractionDigits: 0
                         });
                     }
-
-                    // Si no es un número válido, mostrar el valor original sin formato
                     return precio;
                 },
                 calcularDescuento: function(precioOriginal, porcentajeDescuento) {
@@ -93,7 +86,6 @@
                 },
                 addToCart() {
                     this.loadCart();
-                    // Agregar producto al carrito
                     const existe = this.cart.findIndex((car) => car.id == this.producto.id)
                     console.log(this.existe)
 
@@ -111,18 +103,15 @@
                     this.saveCart();
                 },
                 saveCart() {
-                    // Guardar el carrito en localStorage
                     localStorage.setItem('cart', JSON.stringify(this.cart));
                 },
                 loadCart() {
-                    // Cargar el carrito desde localStorage
                     const savedCart = localStorage.getItem('cart');
                     this.cart = savedCart ? JSON.parse(savedCart) : [];
                 },
-                removeFromCart(item) {
-                    // Quitar producto del carrito
+                removeFromCart(producto) {
                     this.cart = this.cart.filter(product => product.id !== item.id);
-                    // Guardar el carrito en localStorage
+                    console.log(this.cart)
                     this.saveCart();
                 },
                 truncateText(text, maxLength) {
@@ -131,6 +120,14 @@
                     } else {
                         return text;
                     }
+                },
+                decrementarCantidad(producto) {
+                    if (producto.cantidad > 1) {
+                        producto.cantidad--;
+                    }
+                },
+                incrementarCantidad(producto) {
+                    producto.cantidad++;
                 },
             }
         });
